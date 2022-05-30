4bfd6ac25162b045b63b4a87182b7e084ce9aa51
ThomasSchulze@de.ibm.com <ThomasSchulze@de.ibm.com>
2022-05-30 16:42:07 +0200

    updating scripts

--- cp4ba-21-0-3/scripts/deployment-db2-cp4ba/02-createDb2OnOCP.sh
+++ cp4ba-21-0-3/scripts/deployment-db2-cp4ba/02-createDb2OnOCP.sh
@@ -303,15 +303,6 @@ oc exec c-db2ucluster-db2u-0 -it -c db2u -- su - $db2AdminUserName -c "set CUR_C
 sleep 30 #let DB2 settle down
 
 echo
-echo "Removing BLUDB from system."
-oc exec c-db2ucluster-db2u-0 -it -c db2u -- su - $db2AdminUserName -c "db2 force application all"
-sleep 30 #let DB2 settle down
-oc exec c-db2ucluster-db2u-0 -it -c db2u -- su - $db2AdminUserName -c "db2 deactivate database BLUDB"
-sleep 30 #let DB2 settle down
-oc exec c-db2ucluster-db2u-0 -it -c db2u -- su - $db2AdminUserName -c "db2 drop database BLUDB"
-sleep 30 #let DB2 settle down
-
-echo
 echo "Restarting DB2 instance."
 oc exec c-db2ucluster-db2u-0 -it -c db2u -- su -c "sudo wvcli system disable"
 sleep 30 #let DB2 settle down
@@ -332,6 +323,15 @@ echo
 echo "*********************************************************************************"
 echo "********* Installation and configuration of DB2 completed successfully! *********"
 echo "*********************************************************************************"
+
+echo
+echo "Removing BLUDB from system."
+oc exec c-db2ucluster-db2u-0 -it -c db2u -- su - $db2AdminUserName -c "db2 force application all"
+sleep 30 #let DB2 settle down
+oc exec c-db2ucluster-db2u-0 -it -c db2u -- su - $db2AdminUserName -c "db2 deactivate database BLUDB"
+sleep 30 #let DB2 settle down
+oc exec c-db2ucluster-db2u-0 -it -c db2u -- su - $db2AdminUserName -c "db2 drop database BLUDB"
+sleep 30 #let DB2 settle down
 
 echo
 echo "Existing databases are:"
