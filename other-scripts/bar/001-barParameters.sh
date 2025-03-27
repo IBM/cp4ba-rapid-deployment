b5a0ccfd854066a026c554eabe315a432317a916
ThomasSchulze@de.ibm.com <ThomasSchulze@de.ibm.com>
2025-03-27 08:56:11 -0500

    updating bar scripts

--- other-scripts/bar/001-barParameters.sh
+++ other-scripts/bar/001-barParameters.sh
@@ -23,15 +23,15 @@ echo "  Reading 001-barParameters.sh ..."
 # --- Provide those BEFORE running any other bar script ---
 
 # OCP project name for CP4BA to backup/restore, for example ibm-cp4ba
-cp4baProjectName=ibm-cp4ba
+cp4baProjectName=REQUIRED
 
 # If URLs are protected, provide here the information to be used to get the authorization token, if a token is not needed, remove REQUIRED
 useTokenForInsightsengineManagementURL=false
 useTokenForElasticsearchRoute=false
-barTokenUser=
-barTokenPass=
-barTokenResolveCp4ba=
-barCp4baHost=
+barTokenUser=REQUIRED
+barTokenPass=REQUIRED
+barTokenResolveCp4ba=REQUIRED
+barCp4baHost=REQUIRED
 
 # Name of a directory, in which the persistent volume backups should be stored on the storage server
 pvBackupDirectory="\$HOME/backup"
@@ -52,6 +52,10 @@ barConfigData=()
 barStorageClass[0]="nfs-client"
 barMethod[0]="ServerBackup"
 barConfigData[0]='{ "rootDirectory": "/export"}'
+
+barStorageClass[1]="nfs-client-fast"
+barMethod[1]="ServerBackup"
+barConfigData[1]='{ "rootDirectory": "/faststorage"}'
 
 
 
