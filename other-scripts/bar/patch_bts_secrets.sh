b2d9e0a2f60d2b6ed7da0ad5239b87b326074e0d
ThomasSchulze@de.ibm.com <ThomasSchulze@de.ibm.com>
2025-05-23 16:17:36 -0500

    updating bar scripts

--- other-scripts/bar/patch_bts_secrets.sh
+++ other-scripts/bar/patch_bts_secrets.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 # Latest set of secrets to keep, must be set before running this script
-dockercfgToKeep=		# for example secret/ibm-bts-sa-dockercfg-f6thh
-tokenToKeep=			# for example secret/ibm-bts-sa-token-pw292
+# Must begin with secret/ - see example values
+dockercfgToKeep=		# example value: secret/ibm-bts-sa-dockercfg-f6thh
+tokenToKeep=			# example value: secret/ibm-bts-sa-token-pw292
 
 if [[ -z $dockercfgToKeep ]] || [[ -z $tokenToKeep ]]; then
   echo "Please set dockercfgToKeep and tokenToKeep!"
@@ -13,13 +14,13 @@ bts_secrets=$(oc get secrets -o name | grep bts)
 
 for s in $bts_secrets; do
   if [[ $s == secret/ibm-bts-sa-dockercfg-* ]];then
-    if [[ $s == $dockercfgToKeep ]];then
+    if [[ "$s" == "$dockercfgToKeep" ]];then
       echo "Skipping" $s
     else
       oc delete $s
     fi
   elif [[ $s == secret/ibm-bts-sa-token-* ]];then
-    if [[ $s == $tokenToKeep ]];then
+    if [[ "$s" == "$tokenToKeep" ]];then
       echo "Skipping" $s
     else
       oc delete $s
